351f090917f1608c1aeb6edf9fcf1bbc1d3163a6
Robert Pecz <rob.pecz@gmail.com>
2024-12-30 14:33:56 +0100

    Add get resources.

---
+++ SWAPI_Test/Urls/URL.cs
@@ -0,0 +1,13 @@
+﻿namespace SWAPI_Test.Urls
+{
+    internal static class URL
+    {
+        public const string RootUrl = "https://www.swapi.tech/api/";
+        public const string Films = "films";
+        public const string People = "people";
+        public const string Planets = "planets";
+        public const string Species = "species";
+        public const string Starships = "starships";
+        public const string Vehicles = "vehicles";
+    }
+}
